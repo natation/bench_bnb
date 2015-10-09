6b768c2e1f6631d1bb7a3ca7ba30601ada992051
Victor <a7lexis@gmail.com>
2015-10-08 23:29:30 -0700

    finish seeding benches and adding dispatcher

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+
+bow_and_arrow = loc1 = [37.791560, -122.389882]
+smitten = loc2 = [37.776417, -122.424375]
+gg_park = loc3 = [37.769429, -122.486053]
+crissy_field = loc4 = [37.804102, -122.463933]
+Bench.create!({description: "Huge bow and arrow nearby", lat: loc1[0], lng: loc1[1]})
+Bench.create!({description: "Great ice cream at smitten", lat: loc2[0], lng: loc2[1]})
+Bench.create!({description: "There's a lot of space in golden gate park", lat: loc3[0], lng: loc3[1]})
+Bench.create!({description: "Good view of the bay and tiburon", lat: loc4[0], lng: loc4[1]})
